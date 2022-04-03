3ac774aa0dab475aec8ab3199638938f443b8e0b
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-04-04 03:34:02 +0600

    add repo and create responsive navbar ,bootstrap install, routerinstall etc

---
+++ src/component/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container,  Nav, Navbar} from 'react-bootstrap';
+import './Header.css'
+
+
+const Header = () => {
+    return (
+        <div >
+           <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">
+      <h2>Logo</h2>
+      </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+    </Nav>
+    <Nav >
+      <Nav.Link href="/about">About</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#pricing">Contact</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </div>
+    );
+};
+
+export default Header;
