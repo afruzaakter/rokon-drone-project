267f8da905ca93eb46b871bd51110139eb2b6a34
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-04-05 11:00:12 +0600

    update 404 not found page

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
             <div className='d-flex mt-5 justify-content-around row'>
                 <div className='col-md-6 col-sm-12'>
                  <h1 className='text-start mt-5'>Rokon Camera Drone <span className='text-color'>Accessories</span> </h1>
-                 <p className='w-75'>Rokon is a Single eCommerce drone Product.It’s a great place to start if you want to make a website for your drone store, electronics store, watches store, headphone store, earphone store, or jewelry store.</p>
+                 <p className='w-75 1h-1'>Rokon is a Single eCommerce drone Product.It’s a great place to start if you want to make a website for your drone store, electronics store, watches store, headphone store, earphone store, or jewelry store.</p>
                  <div className='align-items-start d-flex '>
                      <button className='btn btn-primary w-25 review-btn'>Read More</button>
                  </div>
--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import bg from '../../images/404 image.png'
 const NotFound = () => {
     return (
         <div>
-           <h1>404</h1> 
-           <h3>Oops! Page Not Found</h3>
-           
+           {/* <h1>404</h1> 
+           <h3>Oops! Page Not Found</h3> */}
+           <img className='w-100' src={bg} alt="" />
+           <h4 className='mt-5 mb-3 '>Try Search or go to Home page</h4>
+            <Link to='/home' className='btn btn-primary'>Rokon Home Page</Link>
         </div>
     );
 };
